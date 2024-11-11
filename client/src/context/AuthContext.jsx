3c1b42e635bd879596b5e478fc257d6f77721633
ShahafPinto <shahafchen7@gmail.com>
2024-11-11 09:24:00 +0200

    context, register, login

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,12 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useCallback, useState } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+    const [user, setUser] = useState(null);
+    const [registerInfo, setRegisterInfo] = useState({
+        username: "",
+        instrument: "",
+        password: "",
+    });
+
+    console.log('registerInfo: ',registerInfo);
+
+    const updateRegisterInfo = useCallback((info)=>{
+        setRegisterInfo(info);
+    }, [])
 
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider 
+        value={{ 
+            user,
+            registerInfo,
+            updateRegisterInfo,
+        }}>
         {children}
     </AuthContext.Provider>
   );
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,25 +3,37 @@ import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
 import { AuthContext } from "../context/AuthContext";
 
 const Register = () => {
+  const { registerInfo, updateRegisterInfo } = useContext(AuthContext);
+
   return (
     <Form>
-      <Row style={{
-        height: "100vh",
-        justifyContent: "center",
-        paddingTop: "10vh" 
-      }}>
+      <Row
+        style={{
+          height: "100vh",
+          justifyContent: "center",
+          paddingTop: "10vh",
+        }}
+      >
         <Col xs={6}>
           <Stack gap={3}>
             <h2>Register</h2>
-            <Form.Control type="text" placeholder="Username" />
-            <Form.Control type="text" placeholder="Instrument" />
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="text"
+              placeholder="Username"
+              onChange={(e) =>
+                updateRegisterInfo({...registerInfo, username: e.target.value })
+              }
+            />
+            <Form.Control type="text" placeholder="Instrument" onChange={(e) =>
+                updateRegisterInfo({...registerInfo, instrument: e.target.value })
+              }/>
+            <Form.Control type="password" placeholder="Password" onChange={(e) =>
+                updateRegisterInfo({...registerInfo, password: e.target.value })
+              }/>
             <Button variant="primary" type="submit">
               Register
             </Button>
-            <Alert variant="danger">
-              Error
-            </Alert>
+            <Alert variant="danger">Error</Alert>
           </Stack>
         </Col>
       </Row>
