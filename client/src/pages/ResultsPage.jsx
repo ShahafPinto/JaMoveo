3616a2c57f1e89be12bebeffcf41d1a0523281e2
ShahafPinto <shahafchen7@gmail.com>
2024-11-12 09:06:56 +0200

    results page

--- client/src/pages/ResultsPage.jsx
+++ client/src/pages/ResultsPage.jsx
@@ -1,21 +1,40 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { ListGroup } from 'react-bootstrap';
 
 const ResultsPage = () => {
   const location = useLocation();
   const { songs } = location.state || { songs: [] };
-
+  const navigate = useNavigate();
+  
+  const handleSelectSong = (song) => {
+    navigate(`/live`, { state: { song } });
+    console.log('selected song:', song);
+  }
+  
   return (
-    <div>
-        <h1>Search Results</h1>
+    <div className="container mt-5">
+        <h1 className="text-center text-light mb-4">Search Results</h1>
         {songs.length > 0 ? (
-            <ul>
+            <ListGroup>
                 {songs.map((song, index) => (
-                    <li key={index}>{song.title}</li> // הצגת פרטי השיר בהתאם למבנה
+                    <ListGroup.Item 
+                        key={index}
+                        className="bg-dark text-light mb-3 p-3 rounded shadow-sm"
+                        as="a" 
+                        href={`#song-${index}`} 
+                        style={{ cursor: 'pointer' }}
+                        onClick={() => handleSelectSong(song)}
+                    >
+                        <h4>{`#${index + 1} Song: ${song.file.slice(0, -5)}`}</h4>
+                        <p>Artist: {song.artist || '-'}</p>
+                        <p>Image: {song.image || '-'}</p>
+                    </ListGroup.Item>
                 ))}
-            </ul>
+            </ListGroup>
         ) : (
-            <p>No songs found.</p>
+            <p className="text-center text-light">No songs found.</p>
         )}
     </div>
   )
