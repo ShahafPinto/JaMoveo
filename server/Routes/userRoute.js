7fe48cb6e349d17e1664c73462d3e181e634db5a
ShahafPinto <shahafchen7@gmail.com>
2024-11-24 13:08:37 +0200

    ,

--- server/Routes/userRoute.js
+++ server/Routes/userRoute.js
@@ -2,8 +2,6 @@ const express = require("express");
 const {
   registerUser,
   loginUser,
-  findUser,
-  getUsers,
 } = require("../Controllers/userController");
 
 const router = express.Router();
@@ -11,7 +9,7 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/admin-register", registerUser);
 router.post("/login", loginUser);
-router.get("/find/:userId", findUser); 
-router.get("/all", getUsers);
+//router.get("/find/:userId", findUser); 
+//router.get("/all", getUsers);
 
 module.exports = router;
